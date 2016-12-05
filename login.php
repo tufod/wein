82e787204606d4b4c031cf295add8c5f6e012b67
marnfelt <marnfelt@tln08-PC.itbh.local>
2016-12-05 15:10:13 +0100

    Anfang Login

---
+++ login.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+<title>Weinhandel</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="styles.css" media="screen">
+</head>
+<body>
+    
+<div id="wrapper">
+    <div id="header">
+        <img src="images/in_vino_veritas.png" alt="In Vino Veritas">
+    </div>
+    <div id="navi">
+        <ul>
+            <li><a href="index_1.html">Startseite</a></li> 
+            <li><a></a></li>
+            <li><a></a></li>
+            <li><a></a></li> 
+        </ul>
+         
+    </div>
+    <div id="main">
+        <h1>Login</h1>
+        <form action="login.php" method="post">
+            <table>
+               <tr>
+                
+                </tr>
+                <tr>
+                    <td id="footer">e-Mail adresse:</td><td><input id="footer" type="text" size="20" name="e-mail" value="tina.mueller@gmx.de"></td>
+                </tr>  
+                <tr> 
+                    <td id="footer">password:</td><td><input id="footer" type="text" size="20" name="password" value="tanz"></td>
+                </tr>  
+            <tr>    <tr><td id="footer">Password:</td><Td><input id="footer" type="text" size="20" name="password" value="tanz"></td>
+                </tr>
+            
+            
+            
+            </table>
+            
+            
+            <input class="button" type="submit" value="registry">
+        </form>     
+        <a class="button" href="#">regänderung</a>
+        <a class="button" href="#">login</a>
+    </div>
+    <div id="footer">Fußzeile</div>
+    
+</div>
+    
+</body>
+</html>
+
