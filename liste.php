8ee911ac99a2bfac8790f608bba4aba126aad033
sfrauenderka <sfrauenderka@tln17-PC.itbh.local>
2016-12-06 12:59:33 +0100

    061216

---
+++ liste.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+require_once './biblio.inc.php';
+$template='';
+$titel='Wein Liste';
+load_tpl('wein.tpl');
+$template = str_replace('{title}', $titel, $template);
+$list=list_output('produkt');
+$template = str_replace('{continer}', $list, $template);
+//echo ' ';
+tpl_output();
+
+?>
+
