207639eac158d32e66e3f24b4d7bd037461af5e5
marnfelt <marnfelt@tln08-PC.itbh.local>
2016-12-06 14:41:16 +0100

    regestrierung- tpl 14.40 Uhr 06.12.2016
    add = Session Start

--- regestrierung.php
+++ regestrierung.php
@@ -1,4 +1,12 @@
   <?php  
+  session_start();
+// session_set_cookie_params($lifetime=3);
+//session_status(2);
+echo'Session-Name:',session_name(),'<br>';
+echo'Session-ID (SID):',session_id(),'<br>';
+// echo'session-LifeTime:', session_set_cookie_params(),'<br>';
+echo'session-status:', session_status(),'<br>';
+  
   require_once './biblio.inc.php';
 $template='';
 $titel='regestrierung';
