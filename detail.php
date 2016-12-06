6284a45418049464781c074b8477798d8c32e871
sfrauenderka <sfrauenderka@tln17-PC.itbh.local>
2016-12-06 14:42:52 +0100

    061216

---
+++ detail.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+require_once './biblio.inc.php';
+$template='';
+$title='Wein Details';
+load_tpl('wein.tpl');
+$template = str_replace('{title}', $title, $template);
+tpl_output();
+
+?>
+
