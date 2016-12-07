ffd3c2c65aab420fe9cce8d230de51ed77b25e7a
Sternhaus01 <arnfelt@gmx.de>
2016-12-07 11:11:55 +0100

    login Update 11.05 Uhr   07.12.2016
    erste Komentare

--- login.php
+++ login.php
@@ -1,47 +1,71 @@
- <?php  
+<?php
+/*
+ * Session Start ( Damit die Seite in die Laufende Session eingebunden ist )
+ */
 session_start();
 // session_set_cookie_params($lifetime=3);
 // session_status(3);
-echo'Session-Name:',session_name(),'<br>';
-echo'Session-ID (SID):',session_id(),'<br>';
+echo'Session-Name:', session_name(), '<br>';
+echo'Session-ID (SID):', session_id(), '<br>';
 //echo'session-LifeTime:', session_set_cookie_params(),'<br>';
-echo'session-status:', session_status(),'<br>';
-   
-   /*
-    *  zusätzliche datei die benötigt wird zum ausführen der Seite
-    */
-  require_once './biblio.inc.php';
-$template='';
+echo'session-status:', session_status(), '<br>';
+
+/*
+ *  zusätzliche datei die benötigt wird zum ausführen der Seite
+ */
+require_once './biblio.inc.php';
+
+$template = '';
 /*
  *  Titel der Seite
  */
-$titel='Login';
+$titel = 'Login';
 
 /*
  *  Load des Wein Template 
  */
 load_tpl('wein.tpl');
-/* 
+/*
  * Login Überschrift
  * eMail Adress Zeile
  * Password eingabe Feld
- * Password Check Feld
+ * 
  *  */
-$erge='<div id="main">
-        <h1>Login</h1>
-        <form action="login.php" method="post">
+$kunde = '';
+
+
+/*
+ *  $erge = Seiten inhalt
+ */
+$erge = '<h1>Login</h1>
+        <form action="login_check.inc.php" method="post">
             <table id="login_table">
                 <tr>
-                    <td id="login">e-Mail adresse:</td><td><input id="registry" type="text" size="20" name="e-mail" value="tina.mueller@gmx.de"></td>
+                    <td id="login">e-Mail adresse:</td><td><input id="registry" type="email" size="20" name="e_mail" value="khaled@hotmail.com"></td>
                 </tr>  
                 <tr> 
-                    <td id="login">password:</td><td><input id="registry" type="password" size="20" name="password" value="tanz" ></td>
+                    <td id="login">password:</td><td><input id="registry" type="password" size="20" name="password" value="12345" ></td>
                 </tr>  
                 <tr>
-                    <td id="login"><input class="button" type="submit" value="Login"></td>
-                </tr>
-            </table> 
-               
+                   <td id="login"></td><td id="login"><input class="button" type="submit" value="Login"></td>
+                </tr>';
+/*
+ * Prüfung ob der Benutzer eine exestierende eMail und das richtige Password hat
+ */
+if (isset($_SESSION['id_benutzer'])) {
+    if ($_SESSION['id_benutzer'] == 0) {
+        $erge .= '<tr>
+                   <td id="login"></td><td id="login">Geben Sie bitte die Email und Password!</td>
+                </tr>';
+    }
+}
+
+/*
+ * Hinweis Wenn mann nicht auf der Seite Regestriert ist (link zur regestrierungs.php)
+ * Hinweis wenn man das Password Vergessen hat ()
+ */
+$erge .= '</table> 
+             </form>  
             <table id="login_table">
                 <tr> 
                     <td id="login">Sie sind noch nicht Regestriert dann gehen sie bitte zur <a class="button" href="regestrierung.php">Regestrierung</a></td>
@@ -49,11 +73,20 @@ $erge='<div id="main">
                 <tr>
                     <td id="login"> Sie haben ihr password Vergessen dann bitte hier  <a class="button" href="#">Passwort Anfordern</a></td>
                 </tr>   
-            </table>
-        </form>
-       </div>';
+            </table>';
 /*
  *   Load des container inhaltes
+ */
+$template = str_replace('{kunde}', $kunde, $template);
+
+/*
+ *  Ausgabe des Titels der Seite
+ */
+$template = str_replace('{title}', $titel, $template);
+
+/*
+ *   Load des Seiten Inhaltes (container)
+ *   Seiten inhalt = $erge
  */
 $template = str_replace('{continer}', $erge, $template);
 tpl_output();
