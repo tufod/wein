ccc144bcb91d6911066191346aecfd6a1c4b1ac7
marnfelt <marnfelt@tln08-PC.itbh.local>
2016-12-07 12:48:31 +0100

    liste Update 07.12.2016  12.47 Uhr Kommentare Update 1

--- liste.php
+++ liste.php
@@ -1,11 +1,48 @@
 <?php
+ /*
+  * Session Start ( Damit die Seite in die Laufende Session eingebunden ist )
+  */
 session_start();
+
+/*
+ *  zusätzliche datei die benötigt wird zum ausführen der Seite
+ */
 require_once './biblio.inc.php';
+
 $template='';
+$kunde=login_check();
+
+/*
+ *  Titel der Seite
+ */
 $title='Wein Liste';
+
+/*
+ *   Load des Seiten Aufbau Templates (Wein Template)
+ *   Seiten Backround Bild unf vorgaben
+ */
 load_tpl('wein.tpl');
+
+/*
+ *   Titel Anzeige der Seite
+ */
 $template = str_replace('{title}', $title, $template);
-$list=list_output('produkt');
+
+/*
+ * Anzeige Kunde rechts oben auf der Seite
+ */
+$template = str_replace('{kunde}', $kunde, $template);
+
+/*
+ *   Load  ... (produkt)
+ *  
+ */
+$list=list_output('produkt'); 
+
+/*
+ *   Load des Seiten Inhaltes (container)
+ *   Seiten inhalt = $erge
+ */
 $template = str_replace('{container}', $list, $template);
 //echo ' ';
 tpl_output();
