6f9c03d22a6818523f0a244f14605cf62bc01b05
Sternhaus01 <arnfelt@gmx.de>
2016-12-07 11:13:36 +0100

    Check in Regestrierung 23.35 Uhr 06.12.2016

---
+++ regestrierung_check.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+//ini_set('session.use_trans_sid', 1);
+session_start();
+require_once './biblio.inc.php';
+$benutzer = benutzer_Email_check($_POST['email']);
+if ($benutzer['id_benutzer'] != 0) {
+   $_SESSION['id_benutzer'] = 0;
+    header('Location: login.php');
+    exit;
+} else {
+   
+    header('Location: liste.php');
+    exit;
+}
+?>
