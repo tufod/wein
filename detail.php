f349ba7adfd48b71c49dece7d8c17dbad195869f
sfrauenderka <sfrauenderka@tln17-PC.itbh.local>
2016-12-07 11:25:05 +0100

    0712161125

--- detail.php
+++ detail.php
@@ -4,8 +4,9 @@ require_once './biblio.inc.php';
 $template='';
 $title='Wein Details';
 load_tpl('wein.tpl');
+$detail = display_detail();
 $template = str_replace('{title}', $title, $template);
+$template = str_replace('{container}',$detail,$template);
 tpl_output();
-$template = diplay_detail('{container}');
 ?>
 
