0860fd43ecbc2438ea23c007b86a848f1a5883ff
Sternhaus01 <arnfelt@gmx.de>
2016-12-07 16:15:21 +0100

    Aktuelle Version Update 0 16.15 Uhr - 07.12.2016

--- liste.php
+++ liste.php
@@ -5,7 +5,7 @@
 session_start();
 
 /*
- *  zusÃ¤tzliche datei die benÃ¶tigt wird zum ausfÃ¼hren der Seite
+ *  zusätzliche datei die benötigt wird zum ausführen der Seite
  */
 require_once './biblio.inc.php';
 
