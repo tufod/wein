813649c1fa4593dcb8525a7c5797c6ab4e8596c9
Sternhaus01 <arnfelt@gmx.de>
2016-12-08 10:06:39 +0100

    Aktuelle Version Update 1 18.00 Uhr - 07.12.2016

--- regestrierung_check.inc.php
+++ regestrierung_check.inc.php
@@ -5,7 +5,7 @@ session_start();
 require_once './biblio.inc.php';
 $benutzer = benutzer_Email_check($_POST['email']);
 if ($benutzer['id_benutzer'] > 0) {
-   $_SESSION['id_benutzer'] = 0;
+   $_SESSION['id_benutzer'] = 'brief';
     header('Location: regestrierung.php');
     exit;
 } else {
