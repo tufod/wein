5e606ef081a9ac9b1d4ad91a731bb9182032281e
Sternhaus01 <arnfelt@gmx.de>
2016-12-09 12:58:37 +0100

    Aktuelle Version Update 3 12.30 Uhr - 09.12.2016

--- regestrierung_check.inc.php
+++ regestrierung_check.inc.php
@@ -5,81 +5,87 @@ session_start();
 require_once './biblio.inc.php';
 $benutzer = benutzer_Email_check($_POST['email']);
 if ($benutzer['id_benutzer'] > 0) {
-    $_SESSION['id_benutzer'] .= ',email';
+    $_SESSION['id_benutzer'] = ',email_falsch';
     header('Location: regestrierung.php');
     exit;
-} else {
+}
+else {
+    $_SESSION['id_benutzer']='';
     $anrede = $_POST['anrede'];
     $vorname = $_POST['vorname'];
-    $pattern = '[a-zA-Z .`-\´öäü]*';
-    $vorname_pruefen = preg_match($pattern, $vorname);
-    if (!$vorname_pruefen) {
-        $_SESSION['id_benutzer'] .= ',vorname';
+   if (preg_match("/^[a-zA-Z .`-\´öäü]*$/", $vorname)==0) {
+        $_SESSION['id_benutzer'] = ',vorname_falsch';
     }
 
     $nachname = $_POST['nachname'];
-    $nachname_pruefen = preg_match($pattern, $nachname);
-    if (!$nachname_pruefen) {
-        $_SESSION['id_benutzer'] .= ',nachname';
+    if ( preg_match("/^[a-zA-Z .`-\´öäü]*$/", $nachname)==0) {
+       $_SESSION['id_benutzer'] .= ',nachname_falsch';
     }
+    
 
     $geburtsdatum_jahr = $_POST['geburtsdatum_jahr'];
     $geburtsdatum_monat = $_POST['geburtsdatum_monat'];
     $geburtsdatum_tag = $_POST['geburtsdatum_tag'];
     $datum_pruefen = datum_pruefen($geburtsdatum_jahr, $geburtsdatum_monat, $geburtsdatum_tag);
     if (!$datum_pruefen) {
-        $_SESSION['id_benutzer'] .= ',datum';
+        $_SESSION['id_benutzer'] .= ',datum_falsch';
     }
 
     $ort = $_POST['ort'];
-    $ort_pruefen = preg_match($pattern, $ort);
-    if (!$ort_pruefen) {
-        $_SESSION['id_benutzer'] .= ',ort';
+    if ( preg_match("/^[a-zA-Z .`-\´öäü]*$/", $ort)==0) {
+        $_SESSION['id_benutzer'] .= ',ort_falsch';
     }
 
 
     $strasse = $_POST['strasse'];
-    $strasse_pruefen = preg_match($pattern, $strasse);
-    if (!$strasse_pruefen) {
-        $_SESSION['id_benutzer'] .= ',strasse';
+   
+    if ( preg_match("/^[a-zA-Z öä.ü-]*$/", $strasse)==0) {
+        $_SESSION['id_benutzer'] .= ',strasse_falsch';
     }
 
-    $pattern = '[0-9]+[a-z]*';
+   
     $hausnummer = $_POST['hausnummer'];
-    $hausnummer_pruefen = preg_match($pattern, $hausnummer);
-    if (!$hausnummer_pruefen) {
-        $_SESSION['id_benutzer'] .= ',hausnummer';
+    
+    if ( preg_match("/^[0-9]+[a-z]*$/", $hausnummer) == 0) {
+        $_SESSION['id_benutzer'] .= ',hausnummer_falsch';
+    }
+    
+    
+    $ort = $_POST['ort'];
+    
+    if ( preg_match("/^[a-zA-Z .`-\´öäü]*$/", $ort) == 0) {
+        $_SESSION['id_benutzer'] .= ',ort_falsch';
     }
 
-    $pattern = '[0-9]{0,9}';
-    $plz = $_POST['plz'];
-    $plz_pruefen = preg_match($pattern, $plz);
-    if (!$plz_pruefen) {
-        $_SESSION['id_benutzer'] .= ',plz';
+   
+    $plz = $_POST['plz'];  
+    if ( preg_match("/^[0-9]{0,9}$/", $plz) ==0) {
+        $_SESSION['id_benutzer'] .= ',plz_falsch';
     }
 
-    $pattern = '[0-9]{0,15}';
-    $telefon = $_POST['telefon'];
-    $telefon_pruefen = preg_match($pattern, $telefon);
-    if (!$telefon_pruefen) {
-        $_SESSION['id_benutzer'] .= ',telefon';
+  
+    $telefon = $_POST['telefon'];   
+    if ( preg_match("/^[0-9]{0,15}$/", $telefon) ==0) {
+        $_SESSION['id_benutzer'] .= ',telefon_falsch';
     }
 
     $email = $_POST['email'];
     $password1 = $_POST['password1'];
     $password2 = $_POST['password2'];
-    $vergleichung = password_vergleichen($pass1, $pass2);
+    $vergleichung = password_vergleichen($password1, $password2);
     if (!$vergleichung) {
-        $_SESSION['id_benutzer'] .= ',password';
+        $_SESSION['id_benutzer'] .= ',password_falsch';
     }
     
-    if($_SESSION['id_benutzer']>0){
+    if($_SESSION['id_benutzer'] !=''){
         header('Location: regestrierung.php');
         exit;
     }
+//    header('Location: regestrierung.php');
+//        exit;
     $con=con_db();
      $sql='';
-    email2benutzer($id_benutzer, 'aktivation');
+    //email2benutzer($id_benutzer, 'aktivation');
    
     uncon_db($con);
 }
