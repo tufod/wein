f67e8b3587db8a4f7824ae194ce306ac2940e64c
Sternhaus01 <arnfelt@gmx.de>
2016-12-09 10:09:35 +0100

    Aktuelle Version Update 3 10.00 Uhr - 09.12.2016

--- biblio.inc.php
+++ biblio.inc.php
@@ -61,41 +61,93 @@ function login_check() {
 
 function filtrator($filter) {
     $filtrator="";
-    if(isset($filter['name'])) {
-        $filtrator.=' produkt_name LIKE "%'.$filter['name'].'%"';
-    }
-    if(isset($filter['weingut'])) {
-        $filtrator.=' weingut = '.$filter["weingut"];
-    }
-    if(isset($filter['land'])) {
-        $filtrator.=' land = '.$filter["land"];
-    }
-    
+    $filtrator.=filtrator_child($filtrator,$filter,'produkt_name');
+    $filtrator.=filtrator_child($filtrator,$filter,'produkt_volumen');
+    $filtrator.=filtrator_child($filtrator,$filter,'name_weintyp');
+    $filtrator.=filtrator_child($filtrator,$filter,'name_weingut');
+    $filtrator.=filtrator_child($filtrator,$filter,'land_name');
+    $filtrator.=filtrator_child($filtrator,$filter,'name_region');
+    $filtrator.=filtrator_child($filtrator,$filter,'name_kontinent');
     return $filtrator;
 }
 
+//feld kann sein produkt_name,produkt_volumen,name_weintyp,name_weingut,land_name,name_region,name_kontinent
+function filtrator_child($filtrator,$filter,$feld) {
+    $filtrator_child='';
+    if((strlen($filtrator)>0) && (strlen($filter[$feld])>0)) {
+        $filtrator_child.=" AND ";
+    }
+    if ((isset($filter[$feld])) && (strlen($filter[$feld])>0)) {
+        $filtrator_child.=$feld.' LIKE ';
+        $filtrator_child.='"%'.$filter[$feld].'%"';
+        }
+    return $filtrator_child;
+}
 
-function list_output($table,$list,$input_filter) {
+function filter_div($input_filter) {
+    $filter_form='<div class="pro">';
+    $filter_form.='<form action="./liste.php" method="GET">';
+    $filter_form.=' Name '.generate_html_form_datalist('produkt_name');
+    $filter_form.=' Typ '.generate_html_form_datalist('name_weintyp');
+    $filter_form.=' Weingut '.generate_html_form_datalist('name_weingut');
+    $filter_form.=' Volume '.generate_html_form_datalist('produkt_volumen');
+    $filter_form.=' Land '.generate_html_form_datalist('land_name');
+    $filter_form.=' Region '.generate_html_form_datalist('name_region');
+    $filter_form.=' Kontinent '.generate_html_form_datalist('name_kontinent');
+    $filter_form.='<input type="submit" value="filter">';
+    $filter_form.='</form></div>';
+    return $filter_form;
+}
+
+//$feld kann sein produkt_name,produkt_volumen,name_weintyp,name_weingut,land_name,name_region,name_kontinent
+function generate_html_form_datalist($feld) {
     $con = con_db();
-    $filter="";
-    if(isset($input_filter['filter'])) {
-            $filter.= " WHERE";
-            $filter.=filtrator($input_filter);
-    }
-    if ($table == 'produkt') {
-        $sql = 'SELECT produkt_nummer,produkt_name,produkt_beschr,'
-                . 'land_name,'
-                . 'produkt_preis,produkt_volumen,LOWER(land_id) AS land_id '
-                . 'FROM produkt '
-                . 'JOIN weingut ON weingut_id = id_weingut '
-                . 'JOIN laender ON id_land = land_id '
-                . ' LIMIT 20';
-    } else {
-        
-    }
-    echo $sql;
+    $select='<input list="'.$feld.'" name="'.$feld.'"><datalist id="'.$feld.'">';
+    $sql = 'SELECT DISTINCT '.$feld.' '
+            . 'FROM produkt p JOIN weintyp w ON p.weintyp_id=w.id_weintyp JOIN weingut wg ON '
+            . 'p.weingut_id=wg.id_weingut JOIN laender l ON wg.land_id=l.id_land JOIN region r ON'
+            . ' wg.region_id=r.id_region JOIN kontinent k ON l.kontinent_id=k.id_kontinent ORDER BY '.$feld.' ASC';
     $res = mysqli_query($con, $sql);
-    $list = '';
+    while ($zeil = mysqli_fetch_assoc($res)) {
+        $select.='<option value="'.$zeil["$feld"].'">';
+    }
+    $select.='</datalist>';
+return $select; 
+}
+
+//$feld kann sein produkt_name,produkt_volumen,name_weintyp,name_weingut,land_name,name_region,name_kontinent
+function generate_html_form_select($feld) {
+    $con = con_db();
+    $select='<select>';
+    $sql = 'SELECT DISTINCT '.$feld.' '
+            . 'FROM produkt p JOIN weintyp w ON p.weintyp_id=w.id_weintyp JOIN weingut wg ON '
+            . 'p.weingut_id=wg.id_weingut JOIN laender l ON wg.land_id=l.id_land JOIN region r ON'
+            . ' wg.region_id=r.id_region JOIN kontinent k ON l.kontinent_id=k.id_kontinent ORDER BY '.$feld.' ASC';
+    $res = mysqli_query($con, $sql);
+    while ($zeil = mysqli_fetch_assoc($res)) {
+        $select.='<option value="'.$zeil["$feld"].'">'.$zeil["$feld"].'</option>';
+    }
+    $select.='</select>';
+return $select; 
+}
+function list_output($input_filter) {
+    $con = con_db();
+    $list="";
+    $filter="";
+    $list.=filter_div($input_filter);
+    if(array_filter($input_filter)) {
+            $filter.= " WHERE ";
+            $filter.=filtrator($input_filter);
+            
+    }
+    $sql = 'SELECT produkt_nummer,produkt_name,produkt_beschr,produkt_preis,LOWER(land_id) AS land_id,land_name,produkt_volumen'
+            . ' FROM produkt p JOIN '
+            . 'weintyp w ON p.weintyp_id=w.id_weintyp JOIN weingut wg ON '
+            . 'p.weingut_id=wg.id_weingut JOIN laender l ON wg.land_id=l.id_land JOIN region r ON'
+            . ' wg.region_id=r.id_region JOIN kontinent k ON l.kontinent_id=k.id_kontinent'.$filter;
+    echo $sql;
+    
+    $res = mysqli_query($con, $sql);
     while ($zeil = mysqli_fetch_assoc($res)) {
         $list .= '<div class="pro">';
 
@@ -146,6 +198,7 @@ function tpl_output() {
     echo $template;
 }
 //detail_seite sascha
+
 function display_detail() {
     $id = $_GET['id'];
     $con = con_db();
@@ -154,7 +207,7 @@ function display_detail() {
             . 'produkt_volumen,LOWER(land_id) AS land_id '
             . 'FROM produkt '
             . 'JOIN weingut ON weingut_id=id_weingut '
-            . 'JOIN laender ON id_land = land_id'
+            . 'JOIN laender ON id_land = land_id '
             .' WHERE produkt_nummer = \''.$id.'\';';
     $res = mysqli_query($con, $sql);
     $detail = '';
