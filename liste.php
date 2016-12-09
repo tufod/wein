d01280c1d04eb5546e7b6d7d26e391be4c91b1d5
Sternhaus01 <arnfelt@gmx.de>
2016-12-09 09:28:10 +0100

    Aktuelle Version Update 2 09.30 Uhr - 09.12.2016
    Aktuelle Version Update 2 09.30 Uhr - 09.12.2016

--- liste.php
+++ liste.php
@@ -9,46 +9,49 @@ session_start();
  */
 require_once './biblio.inc.php';
 
-$template='';
 
-$kunde=login_check();
+/*
+ *   Load des Seiten Aufbau Templates (Wein Template)
+ *   Seiten Backround Bild unf vorgaben
+ */
+
+load_tpl('wein.tpl');
+
+
+$template = str_replace('{search}',$search, $template);
 
 /*
  *  Titel der Seite
  */
 $title='Wein Liste';
-
 $search= search_feld();
 
-/*
- *   Load des Seiten Aufbau Templates (Wein Template)
- *   Seiten Backround Bild unf vorgaben
- */  
-load_tpl('wein.tpl');
-
-$template = str_replace('{search}',$search, $template);
 /*
  *   Titel Anzeige der Seite
  */
 $template = str_replace('{title}', $title, $template);
-
+$kunde_Info=login_check();
 /*
  * Anzeige Kunde rechts oben auf der Seite
  */
-$template = str_replace('{kunde}', $kunde, $template);
-
+$template = str_replace('{kunde}', $kunde_Info, $template);
 /*
- *   Load produktlist mit filter 
+ *   Load  ... (produkt)
+ *  
  */
+$liste_container=list_output('produkt'); 
+if(isset($_GET['filter'])) {
+    $list.='';
+    
+    
+}
 $list=list_output($_GET); 
-
 /*
  *   Load des Seiten Inhaltes (container)
  *   Seiten inhalt = $erge
  */
-$template = str_replace('{container}', $list, $template);
+$template = str_replace('{container}', $liste_container, $template);
 //echo ' ';
-
 /*
  * Seiten Ausgabe
  */
