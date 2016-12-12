98b200234377f99ac666a2062cb395dbe0b49097
Sternhaus01 <arnfelt@gmx.de>
2016-12-12 16:33:31 +0100

    Aktuelle Version Update 1 16.30 Uhr - 12.12.2016

--- login_check.inc.php
+++ login_check.inc.php
@@ -1,33 +1,43 @@
 <?php
 /*
-* Session start +
+* Session start
 */
 //ini_set('session.use_trans_sid', 1);
 session_start();
 require_once './biblio.inc.php';
-$benutzer = benutzer_EmailUndPassword_check($_POST['e_mail'], $_POST['password']);
-if ($benutzer['id_benutzer'] == 1) {
-     $_SESSION['id_benutzer'] = $benutzer['id_benutzer'];
-    header('Location: liste.php');
-}
-else {
+
+/*
+ * Prüfung ob der Benutzer eine exestierende eMail und das richtige Password hat
+ * email und ID_benutzer werdern zurück gegeben
+ */
+$email=$_POST['e_mail'];
+$password=$_POST['password'];
+$benutzer = benutzer_EmailUndPassword_check($email, $password);
+
+/*
+ * Prüfung gibt Ergebnis zurück das die eMail bereits in der Datenbank ist
+ */
 if ($benutzer['id_benutzer'] > 0) {
     $_SESSION['id_benutzer'] = $benutzer['id_benutzer'];
-    $_SESSION['anrede'] = $benutzer['anrede'];
     $_SESSION['vorname'] = $benutzer['vorname'];
-    $_SESSION['nachname'] = $benutzer['nachname'];
-    $_SESSION['geburtsdatum'] = $benutzer['geburtsdatum'];
-    $_SESSION['telefon'] = $benutzer['telefon'];
-    $_SESSION['email'] = $benutzer['email'];
-    $_SESSION['rech_adresse_id'] = $benutzer['rech_adresse_id'];
-    $_SESSION['lief_adresse_id'] = $benutzer['lief_adresse_id'];
+    
+   /*
+    * Leitet die ergebnisse (vorname, id_benutzer) in die (liste.php)
+    */
     header('Location: liste.php');
     exit;
-} 
-else {
+    
+    /*
+     * fehler Pfüfung (eMail Vorhanden)
+     */
+} else {
+    $_SESSION['fehler'] = 'Email';
     $_SESSION['id_benutzer'] = 0;
+    
+    /*
+    * Leitet die ergebnisse (fehler eMail, id_benutzer(= 0)) in die (login.php)
+    */
     header('Location: login.php');
     exit;
-    }
 }
 ?>
