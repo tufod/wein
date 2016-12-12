1f90e6a474a01b67bb64bb3f093c4612fe17dcc2
Sternhaus01 <arnfelt@gmx.de>
2016-12-12 16:33:41 +0100

    Aktuelle Version Update 1 16.30 Uhr - 12.12.2016

--- login.php
+++ login.php
@@ -27,26 +27,46 @@ $template = str_replace('{kunde}', $kunde_Info, $template);
 /*
  *  $erge = Seiten inhalt
  */
-$login_container = '<h1>Login</h1>
+$login_container = '<div id="kunden_login_main">
+    <h1>Login</h1>
         <form action="login_check.inc.php" method="post">
-            <table id="login_table">
+            <table id="kunden_login_main_table">
                 <tr>
-                    <td id="login">e-Mail adresse:</td><td><input id="registry" type="email" size="20" name="e_mail" value=""></td>
+                    <td id="login">
+                    e-Mail adresse:
+                    </td>
+                    <td>
+                    <input id="registry" type="email" size="20" name="e_mail" value="">
+                    </td>
                 </tr>  
                 <tr> 
-                    <td id="login">password:</td><td><input id="registry" type="password" size="20" name="password" value="" ></td>
+                    <td id="login">
+                    password:
+                    </td>
+                    <td>
+                    <input id="registry" type="password" size="20" name="password" value="" >
+                    </td>
                 </tr>  
                 <tr>
-                   <td id="login"></td><td id="login"><input class="button" type="submit" id="login_buttom" value="Login"></td>
+                   <td id="login">
+                   </td>
+                   <td id="login">
+                   <input class="button" type="submit" id="login_buttom" value="Login">
+                   </td>
                 </tr>';
 /*
  * Prüfung ob der Benutzer eine exestierende eMail und das richtige Password hat
  */
-if (isset($_SESSION['id_benutzer'])) {
-    if ($_SESSION['id_benutzer'] == 0) {
+if (isset($_SESSION['fehler'])) {
+    if ($_SESSION['fehler'] =='Email') {
         $login_container .= '<tr>
-                   <td id="login"></td><td id="login">Geben Sie bitte die Email und Password!</td>
+                   <td id="login">
+                   </td>
+                   <td id="login">
+                   Geben Sie bitte die Email und Password!
+                   </td>
                 </tr>';
+        $_SESSION['fehler']='';
     }
 }
 /*
@@ -55,14 +75,15 @@ if (isset($_SESSION['id_benutzer'])) {
  */
 $login_container .= '</table> 
              </form>  
-            <table id="login_table">
+            <table id="kunden_login_main_table">
                 <tr> 
                     <td id="login">Sie sind noch nicht Regestriert dann gehen sie bitte zur <a class="button" href="regestrierung.php">Regestrierung</a></td>
                 </tr>
                 <tr>
                     <td id="login"> Sie haben ihr password Vergessen dann bitte hier  <a class="button" href="#">Passwort Anfordern</a></td>
                 </tr>   
-            </table>';
+            </table>
+            </div>';
 /*
  *   Load des Seiten Inhaltes (container)
  *   Seiten inhalt = $erge
