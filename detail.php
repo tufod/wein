c1822880014fede12d4b4fce521ff087ef05c0d1
Sternhaus01 <arnfelt@gmx.de>
2016-12-12 16:32:25 +0100

    Aktuelle Version Update 1 16.30 Uhr - 12.12.2016

--- detail.php
+++ detail.php
@@ -1,14 +1,12 @@
 <?php
 /*
- * Session Start +
+ * Session Start 
 */
 session_start();
 require_once './biblio.inc.php';
 $template='';
 $title='Wein Details';
-$kunde=login_check();
 load_tpl('wein.tpl');
-$template = str_replace('{kunde}', $kunde, $template);
 $detail = display_detail();
 $template = str_replace('{title}', $title, $template);
 $template = str_replace('{container}',$detail,$template);
