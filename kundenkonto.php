5f4ac96b56992d8853fb9ce07815629e6eaab671
Sternhaus01 <arnfelt@gmx.de>
2016-12-12 09:29:27 +0100

    Aktuelle Version Update 1 09.30 Uhr - 12.12.2016

--- kundenkonto.php
+++ kundenkonto.php
@@ -1,27 +1,18 @@
- <?php
- /*
+<?php
+
+/*
  * Session Start ( Damit die Seite in die Laufende Session eingebunden ist )
  */
-  session_start();
-// session_set_cookie_params($lifetime=3);
-//session_status(2);
-echo'Session-Name:',session_name(),'<br>';
-echo'Session-ID (SID):',session_id(),'<br>';
-// echo'session-LifeTime:', session_set_cookie_params(),'<br>';
-echo'session-status:', session_status(),'<br>';
-/*
- *  zusätzliche datei die benötigt wird zum ausführen der Seite
- */
- 
- require_once './biblio.inc.php';
+session_start();
+require_once './biblio.inc.php';
 
-$template='';
-$kunde_Info=login_check();
+$template = '';
+$kunde_Info = login_check();
 
 /*
  *  Ausgabe des Titels der Seite
  */
-$titel='Kunden Konto';
+$titel = 'Kunden Konto';
 
 /*
  *  Load des Wein Template 
@@ -37,97 +28,254 @@ $template = str_replace('{title}', $titel, $template);
  * Anzeige Kunde rechts oben auf der Seite
  */
 $template = str_replace('{kunde}', $kunde_Info, $template);
-
-
+if (isset($_SESSION['fehler'])) {
+    $fehler = $_SESSION['fehler'];
+} else {
+    $fehler = '';
+}
 
 /*
  *  $erge = Seiten inhalte
  */
-$regestrierung_container='<div id="kunden_konto_main">
+$kundeKonto_container = '<div id="kunden_konto_main">
         
         <h1>Kunden Konto</h1>
     <div id="kunden_konto_leftside">
-        <form action="login.php" method="post">
-       
-            <h2>Kunden Haupt Adresse:</h2>
+        <form action="kundenkonto.inc.php" method="post">       
+            <h2>Haupt Adresse:</h2>
             <table id="registry_table1">
                <tr>
-                <td id="">Anrede:</td> <td id=""> 
+                <td id="">
+                Anrede:
+                </td>
+                <td id=""> 
                 <!-- durch die radio Bottons ist die Auswahl möglich -->
-                <input type="radio" name="anrede" value="female" required> Frau
-                <input type="radio" name="anrede" value="male" required> Herr
-                <input type="radio" name="anrede" value="neutral" required> -
-                </select></td>
+                <input type="radio" name="anrede" value="Frau" required ';
+$kundeKonto_container .= Checked_Anrede_setzen($kundeKonto_container, 'Frau');
+$kundeKonto_container .= '  required="required" > Frau
+                <input type="radio" name="anrede" selected  value="Herr" required  ';
+$kundeKonto_container .= Checked_Anrede_setzen($kundeKonto_container, 'Herr');
+$kundeKonto_container .= '  required="required" > Herr             
+                </td>
+                <td>
+                </td>
                 </tr>
                 <tr>
-                    <td id="registry">Nachname:</td><td><input id="registry" type="text" size="20" name="nachname" value="Müller"></td>
+                    <td id="registry">
+                    Nachname:
+                    </td>
+                    <td>
+                    <input id="registry" type="text" size="20" name="nachname" value="';
+$kundeKonto_container .= Value_Von_Session_setzen($kundeKonto_container, 'nachname');
+$kundeKonto_container .= '"  required="required" >
+                    </td>
+                    <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container, "/Nachname/",$fehler);
+$kundeKonto_container .= '</td>
                 </tr> 
                 <tr>
-                    <td id="registry">Vorname:</td><td><input id="registry" type="text" size="20" name="vorname" value="Tina"></td>
+                    <td id="registry">
+                    Vorname:
+                    </td>
+                    <td>
+                    <input id="registry" type="text" size="20" name="vorname" value="';
+$kundeKonto_container .= Value_Von_Session_setzen($kundeKonto_container, 'vorname');
+$kundeKonto_container .= '"  required="required" >
+                    </td>
+                    <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container, "/Vorname/",$fehler);
+$kundeKonto_container .= '</td>
                 </tr>  
                 <tr>
                 <tr> 
-                    <td id="registry">Geburtsdatum:</td><td><input id="registry" type="text" size="20" name="geburtsdatum" value="24.08.1995"></td>
-                </tr>
+                    <td id="registry">
+                    Geburtsdatum:
+                    </td>
+                    <td>';
+
+$kundeKonto_container .= datumFormola_erzeugen($kundeKonto_container);
+$kundeKonto_container .= '</td>
+                    <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container, "/Geburtsdatum/",$fehler);
+$kundeKonto_container .='</td>
+                 </tr>
+                </table>
+                <h2>Rechnung Adresse:</h2>
+                <table> 
                 <tr>
-                    <td id="registry">Strasse:</td><td><input id="registry" type="text" size="20" name="strasse" value="Bürgermeister-Smidt-Str."></td>
+                    <td id="registry">
+                    Strasse:
+                    </td>
+                    <td>
+                    <input id="registry" type="text" size="20" name="rechnung_strasse" value="';
+$kundeKonto_container .= Value_Von_Session_setzen($kundeKonto_container, 'rechnung_strasse');
+$kundeKonto_container .= '"  required="required" >
+                        </td>
+                        <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container,"/Rechnung Strasse/",$fehler);
+$kundeKonto_container .= '</td>
                 </tr>  
                 <tr>
-                    <td id="registry">Hausnummer:</td><td><input id="registry" type="text" size="20" name="hausnummer" value="31"></td>
-                </tr>  
+                    <td id="registry">
+                    Hausnummer:
+                    </td>
+                    <td>
+                    <input id="registry" type="text" size="20" name="rechnung_hausnummer" value="';
+$kundeKonto_container .= Value_Von_Session_setzen($kundeKonto_container, 'rechnung_hausnummer');
+$kundeKonto_container .= '" required="required" >
+                        </td>
+                <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container,"/Rechnung Hausnummer/",$fehler);
+$kundeKonto_container .= '</td>               
+               </tr>  
                 <tr>
-                    <td id="registry">Ort:</td><td><input id="registry" type="text" size="20" name="ort" value="Bremen"></td>
+                    <td id="registry">
+                    Ort:
+                    </td>
+                    <td>
+                    <input id="registry" type="text" size="20" name="rechnung_ort" value="';
+$kundeKonto_container .= Value_Von_Session_setzen($kundeKonto_container, 'rechnung_ort');
+$kundeKonto_container .= '" required="required" >
+                        </td>
+                        <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container,"/Rechnung Ort/",$fehler);
+$kundeKonto_container .= '</td>
                 </tr> 
                 <tr>
-                    <td id="registry">Plz:</td><td><input id="registry" type="text" size="20" name="plz" value="28195"></td>
+                    <td id="registry">
+                    Plz:
+                    </td>
+                    <td>
+                    <input id="registry" type="text" size="20" name="rechnung_plz" value="';
+$kundeKonto_container .= Value_Von_Session_setzen($kundeKonto_container, 'rechnung_plz');
+$kundeKonto_container .= '" required="required" >
+                        </td>
+                        <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container,"/Rechnung Plz/",$fehler);
+$kundeKonto_container .= '</td>
                 </tr>  
+                
                 <tr>
-                    <td id="registry">Telefon für rückfragen:</td><td><input id="registry" type="text" size="20" name="telefon" value="0421554321"></td>
+                    <td id="registry">
+                    Telefon für rückfragen:
+                    </td>
+                    <td>
+                    <input id="registry" type="text" size="20" name="telefon" value="';
+$kundeKonto_container .= Value_Von_Session_setzen($kundeKonto_container, 'telefon');
+$kundeKonto_container .= '" required="required" >
+                        </td>
+                        <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container,"/Telefon/",$fehler);
+$kundeKonto_container .= '</td>
                 </tr>  
                 <tr> 
-                    <td id="registry">e-Mail adresse:</td><td><input id="registry" type="text" size="20" name="e-mail" value="tina.mueller@gmx.de"></td>
-                </tr>  
-                <tr>
-                    <td ><input class="button" type="submit" id="aendern_button" value="ändern">
-                </tr>
+                    <td id="registry">
+                    e-Mail adresse:
+                    </td>
+                    <td>
+                    <input id="registry" type="text" size="20" name="email" value="';
+$kundeKonto_container .= Value_Von_Session_setzen($kundeKonto_container, 'email');
+$kundeKonto_container .= '" required="required" >
+                        </td>
+                        <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container,"/Email/",$fehler);
+$kundeKonto_container .= '</td>
+                </tr>                  
             </table>
-        </form>
+       
 
  <!-- Passwörter: Kunden Passwort änderungs Bereich -->
-        <form action="login.php" method="post">
-            <h3>Kunden Passwort:</h3>
+    
+            <h3>Passwort:</h3>
             <table id="registry_table1">
                 <tr> 
-                    <td id="registry">password:</td><td><input id="registry" type="text" size="20" name="password" value="tanz"></td>
+                    <td id="registry">
+                    password:
+                    </td>
+                    <td>
+                    <input id="registry" type="password" size="20" name="password1" value="">
+                    </td>
+                    <td>
+                </td>
                 </tr>  
                 <tr>
-                    <td id="registry">Password:</td><Td><input id="registry" type="text" size="20" name="password" value="tanz"></td>
+                    <td id="registry">
+                    Password:
+                    </td>
+                    <td>
+                    <input id="registry" type="password" size="20" name="password2" value="">
+                    </td>
+                    <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container,"/Password/",$fehler);
+$kundeKonto_container .= '</td>
                 </tr>
-                 <tr>
-                    <td id="registry"><input class="button" type="submit" id="aendern_button" value="aendern"></td>
-                </tr>
+                 
             </table>
-        </form>
+       
     </div>
 <!-- Aktive Bestellungen: Kunden Bestellungen die Akiv sind -->
     <div id="kunden_konto_rightside"> 
         <h2>Kunden Liefer Adresse:</h2>
-        <form action="login.php" method="post">
+        
             <table id="registry_table1">
                 <tr>
-                    <td id="registry">Strasse:</td><td><input id="registry" type="text" size="20" name="strasse2" value="Bürgermeister-Smidt-Str."></td>
+                    <td id="registry">
+                    Strasse:
+                    </td>
+                    <td>
+                    <input id="registry" type="text" size="20" name="lieferung_strasse" value="';
+$kundeKonto_container .= Value_Von_Session_setzen($kundeKonto_container, 'lieferung_strasse');
+$kundeKonto_container .= '" required="required" >
+                        </td>
+                        <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container,"/Lieferung Strasse/",$fehler);
+$kundeKonto_container .= '</td>
                 </tr>  
                 <tr>
-                    <td id="registry">Hausnummer:</td><td><input id="registry" type="text" size="20" name="hausnummer2" value="31"></td>
+                    <td id="registry">
+                    Hausnummer:
+                    </td>
+                    <td>
+                    <input id="registry" type="text" size="20" name="lieferung_hausnummer" value="';
+$kundeKonto_container .= Value_Von_Session_setzen($kundeKonto_container, 'lieferung_hausnummer');
+$kundeKonto_container .= '" required="required" >
+                        </td>
+                        <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container,"/Lieferung Hausnummer/",$fehler);
+$kundeKonto_container .= '</td>
                 </tr>  
                 <tr>
-                    <td id="registry">Ort:</td><td><input id="registry" type="text" size="20" name="ort2" value="Bremen"></td>
+                    <td id="registry">
+                    Ort:
+                    </td>
+                    <td>
+                    <input id="registry" type="text" size="20" name="lieferung_ort" value="';
+$kundeKonto_container .= Value_Von_Session_setzen($kundeKonto_container, 'lieferung_ort');
+$kundeKonto_container .= '" required="required" >
+                        </td>
+                        <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container,"/Lieferung Ort/",$fehler);
+$kundeKonto_container .= '</td>
                 </tr> 
                 <tr>
-                    <td id="registry">Plz:</td><td><input id="registry" type="text" size="20" name="plz2" value="28195"></td>
+                    <td id="registry">
+                    Plz:
+                    </td>
+                    <td>
+                    <input id="registry" type="text" size="20" name="lieferung_plz" value="';
+$kundeKonto_container .= Value_Von_Session_setzen($kundeKonto_container, 'lieferung_plz');
+$kundeKonto_container .= '" required="required" >
+                        </td>
+                        <td>';
+$kundeKonto_container .= Element_inArryFehler_Suchen($kundeKonto_container,"/Lieferung Plz/",$fehler);
+$kundeKonto_container .= '</td>
                 </tr>
                 <tr>
-                    <td id="registry"><input class="button" type="submit" id="aendern_button" value="aendern"></td>
+                    <td id="registry">
+                    <input class="button" type="submit" id="aendern_button" value="ändern">
+                    </td>
+                    <td>
+                </td>
                 </tr>
             </table>
             <h2>Aktuelle Bestellungen:</h2>
@@ -139,30 +287,28 @@ $regestrierung_container='<div id="kunden_konto_main">
                     <td id=""></td><td></td>
                 </tr>
                  <tr>
-                    
                 </tr>
             </table>
 <!-- Abgeschlossene Bestellungen: Kunden Bestellungen die Abgeschlossen sind -->
-        <form action="login.php" method="post">
+       
             <h2>Abgeschlossene Bestellungen:</h2>
             <table id="registry_table1">
                 <tr> 
                     <td id=""></td><td></td>
                 </tr>  
-                
                  <tr>
-                    
                 </tr>
             </table>
-                
         </form>
-    </div>';     
-       
+    </div>';
+if (isset($_SESSION['fehler'])) {
+    $_SESSION['fehler'] = '';
+}
 /*
  *   Load des Seiten Inhaltes (container)
  *   Seiten inhalt = $erge
  */
-$template = str_replace('{container}', $regestrierung_container, $template);
+$template = str_replace('{container}', $kundeKonto_container, $template);
 
 /*
  * Seiten Ausgabe
